f808ff1640c80ee34258d5b121e3a71ae2959de9
Harsha5959 <harshakunamneni123@gmail.com>
2025-12-17 09:47:48 +0530

    Fix Render build and start commands

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-python manage.py collectstatic --noinput 
-python manage.py migrate --noinput 
+# python manage.py collectstatic --noinput 
+# python manage.py migrate --noinput 
 gunicorn college_management_system.wsgi:application --bind 0.0.0.0:$PORT 
